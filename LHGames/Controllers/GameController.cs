521e60e5c83988044be53385ee5d025da3a4a366
David <dav_rl@hotmail.com>
2017-10-07 17:09:47 -0400

    final collection

--- LHGames/Controllers/GameController.cs
+++ LHGames/Controllers/GameController.cs
@@ -13,7 +13,8 @@
         {
             Default,
             MoveToRessource,
-            ReturnToHouse
+            ReturnToHouse,
+            MoveToPlayer
         } //sub move action
         enum SubCollectAction
         {
@@ -21,6 +22,15 @@
             CollectRessource,
             DropRessourceToHouse
         } //sub collect action
+        enum SubUpgradeAction
+        {
+            UpgradeMaxHp,
+            UpgradeDmg,
+            UpgradeDef,
+            UpgradeCap,
+            Default
+        }
+
 
 
         [HttpPost]
@@ -28,11 +38,15 @@
         {
             GameInfo gameInfo = JsonConvert.DeserializeObject<GameInfo>(map);
             var carte = AIHelper.DeserializeMap(gameInfo.CustomSerializedMap);
+            string action = string.Empty;
+            ActionTypes PlayerState;
+            SubMoveAction PlayerSubMoveState;
+            ValidateFuturAction();
 
             // INSERT AI CODE HERE.
-            ActionTypes PlayerState = ActionTypes.DefaultAction;
-            SubMoveAction PlayerSubMoveState = SubMoveAction.Default;
+            
             SubCollectAction PlayerSubCollectState = SubCollectAction.Default;
+            //SubUpgradeAction PlayerSubUpgradeState = SubUpgradeAction.Default;
 
             //State machine
             switch (PlayerState)
@@ -43,30 +57,149 @@
                         {
                             case SubMoveAction.MoveToRessource:
                                 {
-                                    double distance = 0;
-                                    double lowestDistance = Int32.MaxValue;
-                                    Point positionToMove = new Point(0, 0);
-                                    for (int i = 0; i < carte.GetLength(0); i++)
+                                    if (gameInfo.Player.CarriedResources >= gameInfo.Player.CarryingCapacity)
                                     {
-                                        for (int j = 0; j < carte.GetLength(1); j++)
+                                        PlayerSubMoveState = SubMoveAction.ReturnToHouse;
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        double distance = 0;
+                                        double lowestDistance = Int32.MaxValue;
+                                        Point positionToMove = new Point(0, 0);
+                                        for (int i = 0; i < carte.GetLength(0); i++)
                                         {
-                                            if (carte[i, j].C == 'R')
+                                            for (int j = 0; j < carte.GetLength(1); j++)
                                             {
-                                                distance = Math.Sqrt(Math.Pow(carte[i, j].X - gameInfo.Player.Position.X, 2) + Math.Pow(carte[i, j].Y - gameInfo.Player.Position.Y, 2));
-                                                if (distance < lowestDistance)
+                                                if (carte[i, j].C == (byte)TileType.R)
                                                 {
-                                                    positionToMove = new Point(carte[i, j].X, carte[i, j].Y);
-                                                    lowestDistance = distance;
+                                                    distance = Math.Sqrt(Math.Pow(carte[i, j].X - gameInfo.Player.Position.X, 2) + Math.Pow(carte[i, j].Y - gameInfo.Player.Position.Y, 2));
+                                                    if (distance < lowestDistance)
+                                                    {
+                                                        positionToMove = new Point(carte[i, j].X, carte[i, j].Y);
+                                                        lowestDistance = distance;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                        if (positionToMove.X > gameInfo.Player.Position.X + 1)
+                                        {
+                                            action = AIHelper.CreateMoveAction(new Point(gameInfo.Player.Position.X + 1, gameInfo.Player.Position.Y));
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            if (positionToMove.X < gameInfo.Player.Position.X - 1)
+                                            {
+                                                action = AIHelper.CreateMoveAction(new Point(gameInfo.Player.Position.X - 1, gameInfo.Player.Position.Y));
+                                                break;
+                                            }
+                                            else
+                                            {
+                                                if (gameInfo.Player.Position.X != positionToMove.X && gameInfo.Player.Position.Y == positionToMove.Y)
+                                                {
+                                                    //test
+                                                    action = AIHelper.CreateCollectAction(positionToMove);
+                                                    break;
+                                                    //PlayerState = ActionTypes.CollectAction;
+                                                    //PlayerSubCollectState = SubCollectAction.CollectRessource;
+                                                }
+                                                else
+                                                {
+                                                    if (gameInfo.Player.Position.X == positionToMove.X && gameInfo.Player.Position.Y != positionToMove.Y)
+                                                    {
+                                                        if (positionToMove.Y > gameInfo.Player.Position.Y + 1)
+                                                        {
+                                                            action = AIHelper.CreateMoveAction(new Point(gameInfo.Player.Position.X, gameInfo.Player.Position.Y + 1));
+                                                            break;
+                                                        }
+                                                        else
+                                                        {
+                                                            if (positionToMove.Y < gameInfo.Player.Position.Y - 1)
+                                                            {
+                                                                action = AIHelper.CreateMoveAction(new Point(gameInfo.Player.Position.X, gameInfo.Player.Position.Y - 1));
+                                                                break;
+                                                            }
+                                                            else
+                                                            {
+                                                                action = AIHelper.CreateCollectAction(positionToMove);
+                                                                break;
+                                                            }
+                                                        }
+                                                    }
+                                                    else
+                                                    {
+                                                        if (positionToMove.Y > gameInfo.Player.Position.Y)
+                                                        {
+                                                            action = AIHelper.CreateMoveAction(new Point(gameInfo.Player.Position.X, gameInfo.Player.Position.Y + 1));
+                                                            break;
+                                                        }
+                                                        else
+                                                        {
+
+                                                            action = AIHelper.CreateMoveAction(new Point(gameInfo.Player.Position.X, gameInfo.Player.Position.Y - 1));
+                                                            break;
+                                                        }
+                                                    }
+
                                                 }
                                             }
                                         }
                                     }
-                                    //pathfiding on passe position to move
-                                    break;
+
+                                    
                                 }
                             case SubMoveAction.ReturnToHouse:
                                 {
-                                    //path fiding code with gameInfo.Player.houseposition
+                                    if (gameInfo.Player.HouseLocation.X > gameInfo.Player.Position.X)
+                                    {
+                                        action = AIHelper.CreateMoveAction(new Point(gameInfo.Player.Position.X + 1, gameInfo.Player.Position.Y));
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        if (gameInfo.Player.HouseLocation.X < gameInfo.Player.Position.X)
+                                        {
+                                            action = AIHelper.CreateMoveAction(new Point(gameInfo.Player.Position.X - 1, gameInfo.Player.Position.Y));
+                                            break;
+                                        }
+                                        else
+                                        {
+                                            if (gameInfo.Player.HouseLocation.Y > gameInfo.Player.Position.Y)
+                                            {
+                                                action = AIHelper.CreateMoveAction(new Point(gameInfo.Player.Position.X, gameInfo.Player.Position.Y + 1));
+                                                break;
+                                            }
+                                            else
+                                            {
+                                                if (gameInfo.Player.HouseLocation.Y < gameInfo.Player.Position.Y)
+                                                {
+                                                    action = AIHelper.CreateMoveAction(new Point(gameInfo.Player.Position.X, gameInfo.Player.Position.Y - 1));
+                                                    break;
+                                                }
+                                                else
+                                                {
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            case SubMoveAction.MoveToPlayer:
+                                {
+                                    double lowestDistance = Int32.MaxValue;
+                                    Point playerLocation = new Point(0, 0);
+                                    foreach(KeyValuePair<string,PlayerInfo> p in gameInfo.OtherPlayers)
+                                    {
+                                        double distance = Math.Sqrt(Math.Pow(p.Value.Position.X - gameInfo.Player.Position.X,2) + Math.Pow(p.Value.Position.Y - gameInfo.Player.Position.Y, 2));
+                                        if (distance < lowestDistance)
+                                        {
+                                            lowestDistance = distance;
+                                            playerLocation = p.Value.Position;
+                                        }
+                                    }
+                                    //pathfinding with playerlocation
+                                    //if arrived action = attackAction
                                     break;
                                 }
                             case SubMoveAction.Default:
@@ -92,31 +225,31 @@
                                     {
                                         Point ressourcePosition = new Point(0, 0);
 
-                                        if (carte[gameInfo.Player.Position.X + 1, gameInfo.Player.Position.Y].C == 'R')
+                                        if (carte[gameInfo.Player.Position.X + 1, gameInfo.Player.Position.Y].C == (byte)TileType.R)
                                         {
                                             ressourcePosition = new Point(gameInfo.Player.Position.X + 1, gameInfo.Player.Position.Y);
                                         }
-                                        if (carte[gameInfo.Player.Position.X - 1, gameInfo.Player.Position.Y].C == 'R')
+                                        if (carte[gameInfo.Player.Position.X - 1, gameInfo.Player.Position.Y].C == (byte)TileType.R)
                                         {
                                             ressourcePosition = new Point(gameInfo.Player.Position.X - 1, gameInfo.Player.Position.Y);
                                         }
-                                        if (carte[gameInfo.Player.Position.X, gameInfo.Player.Position.Y + 1].C == 'R')
+                                        if (carte[gameInfo.Player.Position.X, gameInfo.Player.Position.Y + 1].C == (byte)TileType.R)
                                         {
                                             ressourcePosition = new Point(gameInfo.Player.Position.X, gameInfo.Player.Position.Y + 1);
                                         }
-                                        if (carte[gameInfo.Player.Position.X, gameInfo.Player.Position.Y - 1].C == 'R')
+                                        if (carte[gameInfo.Player.Position.X, gameInfo.Player.Position.Y - 1].C == (byte)TileType.R)
                                         {
                                             ressourcePosition = new Point(gameInfo.Player.Position.X, gameInfo.Player.Position.Y - 1);
                                         }
                                         else
                                         {
-                                            PlayerState = ActionTypes.DefaultAction;
-                                            PlayerSubMoveState = SubMoveAction.Default;
+                                            //PlayerState = ActionTypes.DefaultAction;
+                                            //PlayerSubMoveState = SubMoveAction.Default;
                                             break;
                                         }
 
 
-                                        AIHelper.CreateCollectAction(ressourcePosition);
+                                        action = AIHelper.CreateCollectAction(ressourcePosition);
                                     }
                                     break;
                                 }
@@ -124,6 +257,7 @@
                                 {
                                     //code
                                     AIHelper.CreateCollectAction(gameInfo.Player.Position);
+                                    PlayerState = ActionTypes.DefaultAction;
                                     PlayerSubMoveState = SubMoveAction.Default;
                                     break;
                                 }
@@ -135,19 +269,47 @@
                         }
                         break;
                     }
+                case ActionTypes.AttackAction:
+                    {
+                        for (int i = -1 + gameInfo.Player.Position.X; i < 2 + gameInfo.Player.Position.X; i++)
+                        {
+                            for (int j = -1 + gameInfo.Player.Position.Y; j < 2 + gameInfo.Player.Position.Y; j++)
+                            {
+                                foreach(KeyValuePair<string,PlayerInfo> p in gameInfo.OtherPlayers)
+                                {
+                                    Point attackpoint = new Point(carte[i, j].X, carte[i, j].Y);
+                                    if (p.Value.Position == attackpoint)
+                                    {
+                                        action = AIHelper.CreateAttackAction(attackpoint);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                        PlayerState = ActionTypes.MoveAction;
+                        PlayerSubMoveState = SubMoveAction.MoveToPlayer;
+                        break;
+                    }
                 case ActionTypes.DefaultAction:
                     {
-                        if (gameInfo.Player.CarriedResources < gameInfo.Player.CarryingCapacity)
-                        {
-                            PlayerState = ActionTypes.MoveAction;
-                            PlayerSubMoveState = SubMoveAction.MoveToRessource;
-                        }
                         break;
                     }
             }
 
+            void ValidateFuturAction()
+            {
+                if (gameInfo.Player.CarriedResources >= gameInfo.Player.CarryingCapacity)
+                {
+                    PlayerState = ActionTypes.MoveAction;
+                    PlayerSubMoveState = SubMoveAction.ReturnToHouse;
+                }
+                else
+                {
+                    PlayerState = ActionTypes.MoveAction;
+                    PlayerSubMoveState = SubMoveAction.MoveToRessource;
+                }
+            }
 
-            string action = AIHelper.CreateMoveAction(gameInfo.Player.Position);
             return action;
         }
     }
